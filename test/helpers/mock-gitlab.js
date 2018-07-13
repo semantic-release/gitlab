326ca99c2829c51269113588702a3d863d18c8e1
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-07-13 01:43:33 -0400

    feat: use `cwd` and `env` options passed by core
    BREAKING CHANGE: require `semantic-release` >= `15.8.0`

--- test/helpers/mock-gitlab.js
+++ test/helpers/mock-gitlab.js
@@ -4,19 +4,23 @@ import urlJoin from 'url-join';
 /**
  * Retun a `nock` object setup to respond to a GitLab authentication request. Other expectation and responses can be chained.
  *
- * @param {String} [gitlabToken=process.env.GL_TOKEN || process.env.GITLAB_TOKEN || 'GL_TOKEN'] The github token to return in the authentication response.
- * @param {String} [gitlabUrl=process.env.GL_URL || process.env.GITLAB_URL || 'https://api.github.com'] The url on which to intercept http requests.
- * @param {String} [gitlabApiPathPrefix=process.env.GL_PREFIX || process.env.GITLAB_PREFIX || ''] The GitHub Enterprise API prefix.
+ * @param {Object} [env={}] Environment variables.
+ * @param {String} [gitlabToken=env.GL_TOKEN || env.GITLAB_TOKEN || 'GL_TOKEN'] The github token to return in the authentication response.
+ * @param {String} [gitlabUrl=env.GL_URL || env.GITLAB_URL || 'https://api.github.com'] The url on which to intercept http requests.
+ * @param {String} [gitlabApiPathPrefix=env.GL_PREFIX || env.GITLAB_PREFIX || ''] The GitHub Enterprise API prefix.
  * @return {Object} A `nock` object ready to respond to a github authentication request.
  */
-export default function authenticate({
-  gitlabToken = process.env.GL_TOKEN || process.env.GITLAB_TOKEN || 'GL_TOKEN',
-  gitlabUrl = process.env.GL_URL || process.env.GITLAB_URL || 'https://gitlab.com',
-  gitlabApiPathPrefix = typeof process.env.GL_PREFIX === 'string'
-    ? process.env.GL_PREFIX
-    : null || typeof process.env.GITLAB_PREFIX === 'string'
-      ? process.env.GITLAB_PREFIX
-      : null || '/api/v4',
-} = {}) {
+export default function authenticate(
+  env = {},
+  {
+    gitlabToken = env.GL_TOKEN || env.GITLAB_TOKEN || 'GL_TOKEN',
+    gitlabUrl = env.GL_URL || env.GITLAB_URL || 'https://gitlab.com',
+    gitlabApiPathPrefix = typeof env.GL_PREFIX === 'string'
+      ? env.GL_PREFIX
+      : null || typeof env.GITLAB_PREFIX === 'string'
+        ? env.GITLAB_PREFIX
+        : null || '/api/v4',
+  } = {}
+) {
   return nock(urlJoin(gitlabUrl, gitlabApiPathPrefix), {reqheaders: {'Private-Token': gitlabToken}});
 }
